2abbbd591862b4e22f6178d19cf3e0bb052935d9
Ying Zhao <yzhao@amplify.com>
2014-06-19 16:47:43 -0400

    init commit

---
+++ js/models/model-in-memory.js
@@ -0,0 +1,93 @@
+directory.Employee = Backbone.Model.extend({
+
+    initialize:function () {
+        this.reports = new directory.ReportsCollection();
+        this.reports.parent = this;
+    },
+
+    sync: function(method, model, options) {
+        if (method === "read") {
+            directory.store.findById(parseInt(this.id), function (data) {
+                options.success(data);
+            });
+        }
+    }
+
+});
+
+directory.EmployeeCollection = Backbone.Collection.extend({
+
+    model: directory.Employee,
+
+    sync: function(method, model, options) {
+        if (method === "read") {
+            directory.store.findByName(options.data.name, function (data) {
+                options.success(data);
+            });
+        }
+    }
+
+});
+
+directory.ReportsCollection = Backbone.Collection.extend({
+
+    model: directory.Employee,
+
+    sync: function(method, model, options) {
+        if (method === "read") {
+            directory.store.findByManager(this.parent.id, function (data) {
+                options.success(data);
+            });
+        }
+    }
+
+});
+
+directory.MemoryStore = function (successCallback, errorCallback) {
+
+    this.findByName = function (searchKey, callback) {
+        var employees = this.employees.filter(function (element) {
+            var fullName = element.firstName + " " + element.lastName;
+            return fullName.toLowerCase().indexOf(searchKey.toLowerCase()) > -1;
+        });
+        callLater(callback, employees);
+    }
+
+    this.findByManager = function (managerId, callback) {
+        var employees = this.employees.filter(function (element) {
+            return managerId === element.managerId;
+        });
+        callLater(callback, employees);
+    }
+
+    this.findById = function (id, callback) {
+        var employees = this.employees;
+        var employee = null;
+        var l = employees.length;
+        for (var i = 0; i < l; i++) {
+            if (employees[i].id === id) {
+                employee = employees[i];
+                break;
+            }
+        }
+        callLater(callback, employee);
+    }
+
+    // Used to simulate async calls. This is done to provide a consistent interface with stores that use async data access APIs
+    var callLater = function (callback, data) {
+        if (callback) {
+            setTimeout(function () {
+                callback(data);
+            });
+        }
+    }
+
+    this.employees = [
+    //sample data here
+    ];
+
+    callLater(successCallback);
+
+}
+
+directory.store = new directory.MemoryStore();
